884efc9823f9c03079d8bc8a79ceee98275a7519
Clivon Osire <clivon254@icloud.com>
2025-03-05 12:37:54 +0300

    namna hii

--- server/controller/donationController.js
+++ server/controller/donationController.js
@@ -1,12 +1,120 @@
 
 import Donation from "../model/donationModel.js"
 import { errorHandler } from "../utilis/error.js"
+import axios from "axios"
+
+
+
+let clients = []
+
+
+export const events = (req,res) => {
+
+    res.setHeader('Content-Type', 'text/event-stream')
+
+    res.setHeader('Cache-Control', 'no-cache')
+
+    res.setHeader('Connection', 'keep-alive')
+
+    // Add the client to the list
+    clients.push(res)
+
+    // remove the client the connection is closed
+    req.on('closed',() => {
+
+        clients = clients.filter(client => client !== res)
+
+    })
+
+}
+
+
+// send updates to connected clients
+const sendEventsToClients = (data) => {
+
+    clients.forEach(client => {
+
+        client.write(`data : ${JSON.stringify(data)}\n\n`)
+
+    })
+
+}
+
 
 
 export const mpesaDonation = async (req,res,next) => {
 
+    const {amount,type,category,personalInfo,billingAddress,paymentmethod} = req.body
+
+    const token = req.token
+
+    const phone  = personalInfo.phone.substring(1)
+
     try
-    {}
+    {
+        const donation = new Donation({
+            type,category,personalInfo,billingAddress,paymentmethod
+        })
+
+        await donation.save()
+
+        const date = new Date()
+
+        const timestamp = 
+            date.getFullYear() + 
+            ("0" + (date.getMonth() + 1)).slice(-2) +
+            ("0" + date.getDate()).slice(-2) +
+            ("0" + date.getHours()).slice(-2) +
+            ("0" + date.getMinutes()).slice(-2) +
+            ("0" + date.getSeconds()).slice(-2)
+        
+            const shortcode = process.env.PAYBILL
+
+            const passkey = process.env.PASS_KEY
+
+            const password = new Buffer.from(shortcode + passkey + timestamp).toString("base64")
+
+            const url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+
+            const requestBody = {    
+                "BusinessShortCode": shortcode,    
+                "Password": password,    
+                "Timestamp":timestamp,    
+                "TransactionType": "CustomerPayBillOnline",    
+                "Amount": amount,    
+                "PartyA":`254${phone}`,    
+                "PartyB":shortcode,    
+                "PhoneNumber":`254${phone}`,    
+                "CallBackURL": `http://localhost:3000/api/donation/callback?donationId=${donation._id}`,    
+                "AccountReference":"URCHIN'OBC Donation",    
+                "TransactionDesc":"Test"
+             }
+
+            await axios.post(  
+                url,
+                requestBody,
+                {
+                    headers:{
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    }
+                }
+            )
+            .then((response) => {
+
+                let resData = response.data 
+
+                res.status(200).json({success:true , resData})
+            })
+            .catch((err) => {
+
+                console.log("stk push error")
+
+                res.status(400).json({success:false , message:`${err.message}`})
+            })
+            
+
+    }
     catch(error)
     {
         next(error)
@@ -15,13 +123,134 @@ export const mpesaDonation = async (req,res,next) => {
 }
 
 
+
 export const callbackDonation = async (req,res,next) => {
 
+    const {donationId} = req.query
+
     try
-    {}
+    {
+        console.log("callback is working")
+
+        const callbackData = req.body 
+
+        if(!callbackData.Body.stkCallback.CallbackMetadata)
+        {
+            console.log(callbackData.Body)
+
+            // send notification that the stk push has been attended
+            sendEventsToClients({success:true , message:'STK Push has been attended to '})
+
+            res.json("ok")
+
+        }
+        else
+        {
+            const body = req.body.Body.stkCallback.CallbackMetadata
+
+            console.log(body)
+
+            const donation = await Donation.findById(donationId)
+
+            if(!donation)
+            {
+                return next(errorHandler(404 ,"Donation not found"))
+            }
+            else
+            {
+
+                await Donation.findByIdAndUpdate(donationId ,{payment:true})
+
+                console.log("donation updated")        
+
+            }
+
+            // send notification that the stk push has been attended
+            sendEventsToClients({success:true , message:'STK Push has been attended to '})
+
+            res.status(200).json({success:true})
+
+        }
+
+    }
     catch(error)
     {
         next(error)
+    }
+
+}
+
+
+export const confirmDonation = async (req,res,next) => {
+
+    const {donationId} = req.params
+
+    const token = req.token
+
+    try
+    {
+        const auth = "Bearer " + token
+
+        const url = "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query"
+
+        const date = new Date()
+
+        const timestamp = 
+            date.getFullYear() + 
+            ("0" + (date.getMonth() + 1)).slice(-2) +
+            ("0" + date.getDate()).slice(-2) +
+            ("0" + date.getHours()).slice(-2) +
+            ("0" + date.getMinutes()).slice(-2) +
+            ("0" + date.getSeconds()).slice(-2) 
+
+        const shortcode = process.env.PAYBILL
+
+        const passKey = process.env.PASS_KEY
+
+        const password = new Buffer.from(shortcode + passKey + timestamp).toString("base64")
+
+        const requestBody = {    
+            "BusinessShortCode":shortcode,    
+            "Password": password,    
+            "Timestamp":timestamp,    
+            "CheckoutRequestID": req.params.CheckoutRequestID 
+        }
+
+        const response = await axios.post(url , requestBody , {headers:{"Authorization":auth}})
+
+        if(response.data.ResultCode === "0")
+        {
+
+            const donation = await Donation.findById(donationId)
+
+            if(!donation)
+            {
+                return next(errorHandler(404 ,"Donation not found"))
+            }
+            else
+            {
+
+                await Donation.findByIdAndUpdate(donationId ,{payment:true})
+
+                console.log("donation updated")        
+
+            }
+            
+        }
+        else
+        {
+            await Donation.findByIdAndDelete(donationId)
+
+            res.status(200).json({success:true , data:response.data , message:`${response.data.ResultDesc}` })
+
+        }
+
+    }
+    catch(error)
+    {
+        next(error)
+
+        console.log(error.message)
     }
 
 }
@@ -37,6 +266,7 @@ export const cardDonation = async (req,res,next) => {
     }
 
 }
+
 
 
 export const getDonation = async (req,res,next) => {
@@ -63,6 +293,7 @@ export const getDonation = async (req,res,next) => {
 }
 
 
+
 export const getDonations = async (req,res,next) => {
 
     if(!req.user.isAdmin)
@@ -83,6 +314,7 @@ export const getDonations = async (req,res,next) => {
     }
 
 }
+
 
 
 export const updateDonation = async (req,res,next) => {
@@ -126,6 +358,7 @@ export const updateDonation = async (req,res,next) => {
     }
 
 }
+
 
 
 export const deleteDonation = async (req,res,next) => {
--- server/router/donationRouter.js
+++ server/router/donationRouter.js
@@ -1,10 +1,40 @@
 
 
 import express from "express"
+import { callbackDonation, confirmDonation, deleteDonation, getDonations, mpesaDonation, updateDonation } from "../controller/donationController.js"
+import { generateAccessToken, verifyToken } from "../utilis/verify.js"
+
 
 
 const donationRouter = express.Router()
 
+
+
+donationRouter.post('/mpesa-Donation', generateAccessToken, mpesaDonation)
+
+
+
+donationRouter.post('/callback' , callbackDonation)
+
+
+
+donationRouter.post('/confirm-donation/:donationId/:CheckoutRequestID' ,generateAccessToken , confirmDonation)
+
+
+
+donationRouter.get('/get-donation/:donationId', getDonations)
+
+
+
+donationRouter.get('/get-donations', verifyToken , getDonations)
+
+
+
+donationRouter.put('/update-donation/:donationId',verifyToken , updateDonation)
+
+
+
+donationRouter.delete('/delete-donation/:donationId',verifyToken , deleteDonation)
 
 
 
